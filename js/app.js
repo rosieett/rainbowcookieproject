9b3fbcd2c50bb2250e8b3328530db1d4724b55bd
Rosie Ettenheim <rosie.ettenheim@wsj.com>
2024-06-06 11:57:42 -0400

    handlebars

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
+// import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
-let chart = d3.select('body')
-    .append("div")
-    .classed('smallMultiple', true)
-    .append("svg")
-    .attr('width', '100%')
-    .attr("height", "100%")
-    .style('background-color', 'blue')
+// let chart = d3.select('body')
+//     .append("div")
+//     .classed('smallMultiple', true)
+//     .append("svg")
+//     .attr('width', '100%')
+//     .attr("height", "100%")
+//     .style('background-color', 'blue')
 
 
-const width = chart.svg.node().getBoundingClientRect().width;
-const height = chart.svg.node().getBoundingClientRect().height;
-const marginLeft = 100;
-const marginRight = 50;
-const marginTop = 100;
-const marginBottom = 150;
-const bottom = height - marginBottom;
+// const width = chart.svg.node().getBoundingClientRect().width;
+// const height = chart.svg.node().getBoundingClientRect().height;
+// const marginLeft = 100;
+// const marginRight = 50;
+// const marginTop = 100;
+// const marginBottom = 150;
+// const bottom = height - marginBottom;
+
+
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    var context = 
+    {
+        title: "Hello world",
+        body: "this is the body"
+    }
+    var html = template(context)
+
+    console.log(html);
