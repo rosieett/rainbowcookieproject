a1d035ea6e1d7280bf5cd31211293ec355cfc637
Rosie Ettenheim <rosie.ettenheim@wsj.com>
2024-06-10 16:53:39 -0400

    got the data to load on the page within context variable in app.js but the template is still html on the bakeryprofile.html page

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 import data from "/data.json" with { type: "json" };
 
+
 //get the bakery number for the profile that was selected
 let url = new URL(document.location);
 let params = url.searchParams;
-let bakeryNumber = params.get("bakeryNumber");
+let bakeryID = params.get("bakeryNumber");
 
-//set up a filter to filter the array for objects whos bakery number matches the bakerNumber then grab the first object from that filtered set
+//set up a filter to filter the array for objects whos bakeryID matches the bakeryNumber then grab the first object from that filtered set
+let result = data.filter((b) => b.bakeryNumber == bakeryID)[0]
+console.log(result)
 
-
-
-
-
-
-
-var source = document.getElementById("bakerypage-template").innerHTML;
-
+var source = document.getElementById("entry-template").innerHTML;
 var template = Handlebars.compile(source);
+var context = { 
+    'bakeryhed': result.bakeryhed, 
+    'bakeryaddress': result.bakeryaddress,
+    'ranking': result.Scores[0].Overall,
+    'price': result.price,
+    'bakeryDescription': result.bakeryDescription,
+    
 
-var html = template(data);
-console.log(html)
 
-document.getElementById("bakery-page").innerHTML = html;
+
+
+};
+
+
+var html = template(context);
+
+document.getElementById("profile").innerHTML = html;
+
+
+
+
+
+
+// document.getElementById("bakery-page").innerHTML = html;
+
+// var context = {bakeryhed: "test info"};
+// var html = template(context);
+
 
