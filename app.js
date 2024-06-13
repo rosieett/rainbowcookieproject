339163c5c5893300c0e52c0ce06082d523259791
Rosie Ettenheim <rosie.ettenheim@wsj.com>
2024-06-13 10:50:42 -0400

    working prior to figuring out hbs

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import data from "/data.json" with { type: "json" };
+// import bakeryprofile from "./bakeryprofile.hbs" with { type: "json"}
+// import test from "./test.js" 
+// console.log(test)
+// import hbs from 'hbs'; 
 
 
 //get the bakery number for the profile that was selected
@@ -12,6 +16,8 @@ console.log(result)
 
 
 var source = document.getElementById("entry-template").innerHTML;
+// Handlebars.registerPartial('bakeryprofile', bakeryprofile);
+// console.log(bakeryprofile)
 var template = Handlebars.compile(source);
 var context = result;
 
@@ -20,13 +26,47 @@ var html = template(context);
 document.getElementById("profile").innerHTML = html;
 
 
+//CHART AVERAGE AREA IN d3
+// let width = document.getElementById("chart-average").offsetWidth
 
+// let height = document.getElementById("chart-average").offsetHeight
 
+// var svg = d3.select('#chart-average')
+//     .append('svg')
+//         .attr("width", width)
+//         .attr("height", height)
+//     .append('g')
 
+// var rows = result.avgScores;
+// var value = [0, 1, 2, 3, 4, 5]
 
-// document.getElementById("bakery-page").innerHTML = html;
+// const x = d3.scaleBand()
+//     .range([0, width])
+//     .domain(rows)
+//     .padding(0.01)
+// svg.append("g")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.axisBottom(x))
 
-// var context = {bakeryhed: "test info"};
-// var html = template(context);
+// const y = d3.scaleBand()
+//     .range([height, 0])
+//     .domain([0,5])
+//     .padding(0.01)
+// svg.append("g")
+//     .call(d3.axisLeft(y))
 
-
+// svg.selectAll()
+//     .data(data, function(d) {
+//         return d.row+':'+d.value
+//     })
+//     .enter()
+//     .append("rect")
+//     .attr("x", function(d) {
+//         return x(d.value)
+//     })
+//     .attr("y", function(d) {
+//         return y(d.row)
+//     })
+//     .attr("width", x.bandwidth())
+//     .attr("height", y.bandwidth())
+//     .style("fill", "#dddddd")
