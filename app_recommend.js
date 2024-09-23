0327c399670b1dc33613bfb673a9ee57f033c36a
Rosie Ettenheim <rosie.ettenheim@wsj.com>
2024-09-23 16:55:23 -0400

    fixed modal on mobile (I spelled things right)

--- app_recommend.js
+++ app_recommend.js
@@ -1,12 +1,21 @@
-let form = document.querySelector("#form");
-let nameinput = document.querySelector("#name");
-let sendButton = document.querySelector("#sendButton");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let name = nameinput.value;
-  console.log(name);
-  form.submit();
-});
-
 let submitted = false;
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
