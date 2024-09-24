f13f5a63678522da9b27615de0270b99b2535c42
Rosie Ettenheim <rosie.ettenheim@wsj.com>
2024-09-24 13:50:13 -0400

    mobile menu

--- app_bakeryprofile.js
+++ app_bakeryprofile.js
@@ -161,3 +161,26 @@ const closeModal = function () {
 
 closeModalBtn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const body = document.querySelector("body");
+const backdrop = document.createElement("div");
+backdrop.classList.add("menu-backdrop");
+
+body.appendChild(backdrop);
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  backdrop.classList.toggle("display"); // Show or hide the backdrop
+
+  // Burger Animation
+  burger.classList.toggle("toggle");
+});
+
+backdrop.addEventListener("click", function () {
+  navLinks.classList.remove("nav-active");
+  this.classList.remove("display"); // Hide the backdrop when clicked
+  burger.classList.remove("toggle");
+});
